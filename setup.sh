4402382a2e362efa6905989ec2c443aedbe6d6e2
mfehr <mathieu.fehr@gmail.com>
2021-01-18 20:59:55 +0100

    [mlir] Setup llvm submodule

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p build && cd build
-cmake -G Ninja .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DMLIR_DIR=/home/fehr/prog/llvm-project/build/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=/home/fehr/prog/llvm-project/build/bin/llvm-lit
+cmake -G Ninja .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DMLIR_DIR=$(pwd)/../llvm-project/build/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$(pwd)/../llvm-project/build/bin/llvm-lit
