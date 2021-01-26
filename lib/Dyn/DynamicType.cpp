20763bf8425be024fbbe5cedb27a2047fe8932ab
mfehr <mathieu.fehr@gmail.com>
2021-01-26 21:48:58 +0100

    [dyn] Add suport for dynamic types.
    Dynamic types are defined by DynamicTypeDefinition. Each instance represent
    a different type, and each instance has a differen TypeID.
    
    The types do not have parameters yet, nor custom printer/parser.

---
+++ lib/Dyn/DynamicType.cpp
@@ -0,0 +1,27 @@
+//===- DynamicType.cpp - Dynamic types --------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Dyn/DynamicType.h"
+#include "Dyn/DynamicDialect.h"
+#include "mlir/IR/TypeSupport.h"
+#include "llvm/ADT/ArrayRef.h"
+
+using namespace mlir;
+using namespace dyn;
+
+DynamicTypeDefinition::DynamicTypeDefinition(DynamicDialect *dialect,
+                                             llvm::StringRef name)
+    : DynamicObject(dialect->getDynamicContext()), dialect(dialect),
+      name(name.str()) {}
+
+DynamicTypeDefinition *DynamicType::getTypeDef() { return getImpl()->typeDef; }
+
+DynamicType DynamicType::get(MLIRContext *ctx, DynamicTypeDefinition *typeDef) {
+  return detail::TypeUniquer::getWithTypeID<DynamicType>(
+      ctx, typeDef->getRuntimeTypeID(), typeDef);
+}
