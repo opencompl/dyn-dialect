8b26bb84cee3dc8164adc7a2b41e4da69fdd72d7
mfehr <mathieu.fehr@gmail.com>
2021-02-02 16:39:06 +0100

    [irdl] Register types and operations in irdl files

--- lib/Dyn/Dialect/IRDL/IRDLRegistration.cpp
+++ lib/Dyn/Dialect/IRDL/IRDLRegistration.cpp
@@ -14,16 +14,50 @@
 #include "Dyn/Dialect/IRDL/IR/IRDL.h"
 #include "Dyn/DynamicContext.h"
 #include "Dyn/DynamicDialect.h"
+#include "mlir/Support/LogicalResult.h"
 
 using namespace mlir;
 using namespace irdl;
 
+namespace {
+LogicalResult registerType(TypeOp typeOp, dyn::DynamicDialect *dialect) {
+  return dialect->createAndAddType(typeOp.name());
+}
+
+LogicalResult registerOperation(OperationOp op, dyn::DynamicDialect *dialect) {
+  return dialect->createAndAddOperation(op.name());
+}
+} // namespace
+
 LogicalResult mlir::irdl::registerDialect(DialectOp dialectOp,
                                           dyn::DynamicContext *ctx) {
+
+  // Register the dialect
   auto dialectRes = ctx->createAndRegisterDialect(dialectOp.name());
   if (failed(dialectRes))
     return failure();
 
   auto *dialect = *dialectRes;
-  return dialect->createAndAddOperation("dummyop");
+
+  // Register the types
+  auto failedTypeRegistration = success();
+  dialectOp.walk([&](TypeOp type) {
+    if (failed(registerType(type, dialect)))
+      failedTypeRegistration = failure();
+  });
+
+  if (failed(failedTypeRegistration))
+    return failure();
+
+  // Register the operations
+  auto failedOperationRegistration = success();
+  dialectOp.walk([&](OperationOp op) {
+    if (failed(registerOperation(op, dialect)))
+      failedOperationRegistration = failure();
+  });
+
+  if (failed(failedOperationRegistration))
+    return failure();
+
+  return success();
 }
