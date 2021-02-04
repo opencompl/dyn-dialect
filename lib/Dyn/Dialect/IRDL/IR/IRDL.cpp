4ae1bf62362e1e61481df1e1748010cc759ff622
mfehr <mathieu.fehr@gmail.com>
2021-02-04 17:08:39 +0100

    [irdl] Change format of irdl.operation to include operation type

--- lib/Dyn/Dialect/IRDL/IR/IRDL.cpp
+++ lib/Dyn/Dialect/IRDL/IR/IRDL.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "Dyn/Dialect/IRDL/IR/IRDL.h"
+#include "Dyn/Dialect/IRDL/IR/IRDLAttributes.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/Support/LogicalResult.h"
@@ -23,6 +24,7 @@ void IRDLDialect::initialize() {
 #define GET_OP_LIST
 #include "Dyn/Dialect/IRDL/IR/IRDLOps.cpp.inc"
       >();
+  addAttributes<OpTypeDefAttr>();
 }
 
 //===----------------------------------------------------------------------===//
@@ -86,6 +88,117 @@ static void print(OpAsmPrinter &p, TypeOp typeOp) {
 }
 
 //===----------------------------------------------------------------------===//
+// irdl::OpTypeDefAttr
+//===----------------------------------------------------------------------===//
+
+namespace {
+
+/// Parse a type constraint with the format "dialect.name".
+/// The verifier ensures that the format is respected. Only a keyword is parsed
+/// (since mlir will parse "." in keywords).
+ParseResult parseTypeConstraint(OpAsmParser &p,
+                                TypeConstraint *typeConstraint) {
+  StringRef name;
+
+  if (p.parseKeyword(&name))
+    return failure();
+
+  *typeConstraint = name.str();
+  return success();
+}
+
+/// Print a type constraint with the format "dialect.name".
+void printTypeConstraint(OpAsmPrinter &p,
+                         const TypeConstraint &typeConstraint) {
+  p << typeConstraint;
+}
+
+/// Parse an ArgDef with format "name: typeConstraint".
+ParseResult parseArgDef(OpAsmParser &p, ArgDef *argDef) {
+  StringRef name;
+  TypeConstraint typeConstraint;
+
+  if (p.parseKeyword(&name) || p.parseColon() ||
+      parseTypeConstraint(p, &argDef->second))
+    return failure();
+
+  argDef->first = name.str();
+  return success();
+}
+
+/// Print an ArgDef with format "name: typeConstraint".
+void printTypedVar(OpAsmPrinter &p, const ArgDef &argDef) {
+  p << argDef.first << ": ";
+  printTypeConstraint(p, argDef.second);
+}
+
+/// Parse an ArgDefs with format ([argDef,]*).
+/// The trailing comma is optional.
+ParseResult parseArgDefs(OpAsmParser &p, OwningArgDefs *argDefs) {
+  if (p.parseLParen())
+    return failure();
+
+  while (true) {
+    if (!p.parseOptionalRParen())
+      break;
+
+    ArgDef argDef;
+    if (parseArgDef(p, &argDef))
+      return failure();
+    argDefs->push_back(argDef);
+
+    if (!p.parseOptionalComma())
+      continue;
+
+    if (p.parseRParen())
+      return failure();
+
+    break;
+  }
+
+  return success();
+}
+
+void printArgDefs(OpAsmPrinter &p, ArgDefs typedVars) {
+  p << "(";
+  for (const auto &typedVar : typedVars) {
+    printTypedVar(p, typedVar);
+    p << ", ";
+  }
+  p << ")";
+}
+
+/// Parse an OpTypeDefAttr.
+/// The format is "operandDef -> resultDef" where operandDef and resultDef have
+/// the ArgDefs format.
+ParseResult parseOpTypeDefAttr(OpAsmParser &p, OpTypeDefAttr *opTypeDefAttr) {
+  OwningArgDefs operandDefs, resultDefs;
+  // Parse the operands.
+  if (parseArgDefs(p, &operandDefs))
+    return failure();
+
+  if (p.parseArrow())
+    return failure();
+
+  // Parse the results.
+  if (parseArgDefs(p, &resultDefs))
+    return failure();
+
+  *opTypeDefAttr =
+      OpTypeDefAttr::get(*p.getBuilder().getContext(), std::move(operandDefs),
+                         std::move(resultDefs));
+  return success();
+}
+
+void printOpTypeDef(OpAsmPrinter &p, OpTypeDef opDef) {
+  printArgDefs(p, opDef.operandDef);
+  p << " -> ";
+  printArgDefs(p, opDef.resultDef);
+}
+
+} // namespace
+
+//===----------------------------------------------------------------------===//
 // irdl::OperationOp
 //===----------------------------------------------------------------------===//
 
@@ -94,9 +207,15 @@ static ParseResult parseOperationOp(OpAsmParser &p, OperationState &state) {
 
   // Parse the operation name.
   StringRef name;
-  if (failed(p.parseKeyword(&name)))
+  if (p.parseKeyword(&name))
     return failure();
   state.addAttribute("name", builder.getStringAttr(name));
+
+  // Parse the OpDefAttr.
+  OpTypeDefAttr opTypeDef;
+  if (parseOpTypeDefAttr(p, &opTypeDef))
+    return failure();
+  state.addAttribute("op_def", opTypeDef);
 
   return success();
 }
@@ -106,6 +225,9 @@ static void print(OpAsmPrinter &p, OperationOp operationOp) {
 
   // Print the operation name.
   p << operationOp.name();
+
+  // Print the operation type constraints.
+  printOpTypeDef(p, operationOp.op_def());
 }
 
 //===----------------------------------------------------------------------===//
