11b749dd001a34a327f22336b929a3b2661a92c4
mfehr <mathieu.fehr@gmail.com>
2021-02-04 20:32:43 +0100

    [irdl] Add type verifiers to the dynamic operations
    For now, the only type verifier is the type equality.

--- lib/Dyn/Dialect/IRDL/IRDLRegistration.cpp
+++ lib/Dyn/Dialect/IRDL/IRDLRegistration.cpp
@@ -16,6 +16,7 @@
 #include "Dyn/DynamicDialect.h"
 #include "mlir/IR/Visitors.h"
 #include "mlir/Support/LogicalResult.h"
+#include "llvm/ADT/STLExtras.h"
 
 using namespace mlir;
 using namespace irdl;
@@ -26,9 +27,65 @@ LogicalResult registerType(TypeOp typeOp, dyn::DynamicDialect *dialect) {
   return dialect->createAndAddType(typeOp.name());
 }
 
+/// Objects representing the type constraints of dynamic operations.
+/// Each operand and each result have a name, and a type constraint.
+using OpTypeConstraint = std::pair<std::string, TypeConstraint>;
+using OpTypeConstraints = std::pair<OwningArgDefs, OwningArgDefs>;
+
+LogicalResult verifyOpTypeConstraints(Operation *op,
+                                      const OpTypeConstraints &typeConstrs,
+                                      dyn::DynamicContext &ctx) {
+  auto operandConstrs = typeConstrs.first;
+  auto resultConstrs = typeConstrs.second;
+
+  /// Check that we have the right number of operands.
+  auto numOperands = op->getNumOperands();
+  auto numExpectedOperands = typeConstrs.first.size();
+  if (numOperands != numExpectedOperands)
+    return op->emitOpError(std::to_string(numExpectedOperands) +
+                           " operands expected, but got " +
+                           std::to_string(numOperands));
+
+  /// Check that we have the right number of results.
+  auto numResults = op->getNumResults();
+  auto numExpectedResults = typeConstrs.second.size();
+  if (numResults != numExpectedResults)
+    return op->emitOpError(std::to_string(numExpectedResults) +
+                           " results expected, but got " +
+                           std::to_string(numResults));
+
+  /// Check that all operands satisfy the constraints.
+  for (unsigned i = 0; i < numOperands; ++i) {
+    auto operandType = op->getOperand(i).getType();
+    auto constraint = operandConstrs[i].second;
+    if (failed(constraint.verifyType(op, operandType, i, true, ctx)))
+      return failure();
+  }
+
+  /// Check that all results satisfy the constraints.
+  for (unsigned i = 0; i < numResults; ++i) {
+    auto resultType = op->getResult(i).getType();
+    auto constraint = resultConstrs[i].second;
+    if (failed(constraint.verifyType(op, resultType, i, true, ctx)))
+      return failure();
+  }
+
+  return success();
+}
+
 /// Register an operation represented by a `irdl.operation` operation.
 LogicalResult registerOperation(OperationOp op, dyn::DynamicDialect *dialect) {
-  return dialect->createAndAddOperation(op.name());
+  OpTypeDef opDef = op.op_def();
+  OpTypeConstraints constraints =
+      make_pair(std::vector<ArgDef>(opDef.operandDef),
+                std::vector<ArgDef>(opDef.resultDef));
+
+  auto ctx = dialect->getDynamicContext();
+  auto typeVerifier = [constraints{std::move(constraints)},
+                       ctx](Operation *op) {
+    return verifyOpTypeConstraints(op, constraints, *ctx);
+  };
+  return dialect->createAndAddOperation(op.name(), {typeVerifier});
 }
 } // namespace
 
