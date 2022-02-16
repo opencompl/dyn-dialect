025d2ef36a4544f0235d32eca7ae04683dfe291e
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-02-16 17:42:15 +0100

    Compile in debug mode

--- utils/setup.sh
+++ utils/setup.sh
@@ -2,4 +2,4 @@
 sudo apt-get install ninja-build && ninja --version
 mkdir -p build && cd build
 export CMAKE_GENERATOR=Ninja
-cmake -G Ninja ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DMLIR_DIR=$(pwd)/../llvm-project/build/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$(pwd)/../llvm-project/build/bin/llvm-lit
+cmake -G Ninja ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DMLIR_DIR=$(pwd)/../llvm-project/build/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$(pwd)/../llvm-project/build/bin/llvm-lit
