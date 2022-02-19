c0fbe06ed7a9c46c4a635dbb397ff4650a8cef58
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-02-19 05:36:44 +0100

    Add AtMostOneChildOf trait

--- include/Dyn/Dialect/IRDL/IR/IRDL.h
+++ include/Dyn/Dialect/IRDL/IR/IRDL.h
@@ -13,6 +13,7 @@
 #ifndef DYN_DIALECT_IRDL_IR_IRDL_H_
 #define DYN_DIALECT_IRDL_IR_IRDL_H_
 
+#include "Dyn/Dialect/IRDL/IR/IRDLTraits.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/ExtensibleDialect.h"
---
+++ include/Dyn/Dialect/IRDL/IR/IRDLTraits.h
@@ -0,0 +1,53 @@
+//===- IRDLTraits.h - IRDL traits definition ---------------------*- C++
+//-*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the traits used by the IR Definition Language dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Support/LogicalResult.h"
+#include "llvm/Support/Casting.h"
+
+namespace mlir {
+namespace OpTrait {
+
+/// This class adds the property that there is at most one child of a given op
+/// in the operation region. It also provides an API to retrieve the operation.
+template <typename ChildOp> class AtMostOneChildOf {
+public:
+  template <typename ConcreteType>
+  class Impl
+      : public ::mlir::OpTrait::TraitBase<ConcreteType,
+                                          AtMostOneChildOf<ChildOp>::Impl> {
+  public:
+    static LogicalResult verifyTrait(Operation *op) {
+      static_assert(
+          ConcreteType::template hasTrait<::mlir::OpTrait::OneRegion>(),
+          "expected operation to have a single region");
+
+      auto ops = cast<ConcreteType>(op).template getOps<ChildOp>();
+      return success(ops.empty() || ++ops.begin() == ops.end());
+    }
+
+    /// Get the unique operation of a specific op that is in the operation
+    /// region.
+    template <typename OpT = ChildOp>
+    std::enable_if_t<std::is_same<OpT, ChildOp>::value, llvm::Optional<OpT>>
+    getOp() {
+      auto ops =
+          cast<ConcreteType>(this->getOperation()).template getOps<ChildOp>();
+      if (ops.empty())
+        return {};
+      return {*ops.begin()};
+    }
+  };
+};
+} // namespace OpTrait
+} // namespace mlir
