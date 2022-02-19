e787792439b9683bb6008fab1f98edc4a0eb8287
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-02-19 04:16:56 +0100

    Remove ninja check in utils/setup.sh

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt-get install ninja-build && ninja --version
 mkdir -p build && cd build
 export CMAKE_GENERATOR=Ninja
 cmake -G Ninja ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DMLIR_DIR=$(pwd)/../llvm-project/build/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$(pwd)/../llvm-project/build/bin/llvm-lit
