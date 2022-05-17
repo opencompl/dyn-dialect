0925678ce1cee7a29d890ddac0bb800a71bbd950
Théo Degioanni <theo.degioanni@ens-rennes.fr>
2022-05-17 10:15:00 +0100

    changed llvm build linker detection

--- utils/build-llvm.sh
+++ utils/build-llvm.sh
@@ -15,6 +15,16 @@
 BUILD_DIR=${1:-"build"}
 INSTALL_DIR=${2:-"install"}
 
+if command -v mold > /dev/null; then
+  BEST_LINKER="mold"
+elif command -v lld > /dev/null; then
+  BEST_LINKER="lld"
+else
+  BEST_LINKER="ld"
+fi
+
+LINKER=${3:-${BEST_LINKER}}
+
 mkdir -p llvm-project/$BUILD_DIR
 mkdir -p llvm-project/$INSTALL_DIR
 cd llvm-project/$BUILD_DIR
@@ -29,7 +39,7 @@ cmake -G Ninja ../llvm \
   -DLLVM_INSTALL_UTILS=ON \
   -DCMAKE_C_COMPILER=clang \
   -DCMAKE_CXX_COMPILER=clang++ \
-  -DLLVM_ENABLE_LLD=ON \
+  -DLLVM_USE_LINKER=${LINKER} \
   -DCMAKE_BUILD_TYPE=Debug \
   -DLLVM_ENABLE_ASSERTIONS=ON
 
