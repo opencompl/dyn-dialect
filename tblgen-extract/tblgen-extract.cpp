3c470876ffe920e0c35da98267c3062e0a5b96cb
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-05-26 04:30:14 +0200

    Add boilerplate for tblgen-extract

---
+++ tblgen-extract/tblgen-extract.cpp
@@ -0,0 +1,54 @@
+//===- tblgen-extract.cpp --------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Extract IRDL from a TableGen file.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/TableGen/AttrOrTypeDef.h"
+#include "mlir/TableGen/GenInfo.h"
+#include "mlir/TableGen/GenNameParser.h"
+#include "mlir/TableGen/Interfaces.h"
+#include "mlir/TableGen/Operator.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/TableGen/Main.h"
+#include "llvm/TableGen/Record.h"
+#include "llvm/TableGen/TableGenBackend.h"
+
+using namespace llvm;
+using namespace mlir;
+using namespace mlir::tblgen;
+
+std::vector<Record *> getOpDefinitions(const RecordKeeper &recordKeeper) {
+  return recordKeeper.getAllDerivedDefinitions("Op");
+}
+
+std::vector<Record *> getTypeDefinitions(const RecordKeeper &recordKeeper) {
+  return recordKeeper.getAllDerivedDefinitions("TypeDef");
+}
+
+std::vector<Record *> getAttrDefinitions(const RecordKeeper &recordKeeper) {
+  return recordKeeper.getAllDerivedDefinitions("AttrDef");
+}
+
+bool MlirTableGenStatsMain(raw_ostream &os, RecordKeeper &records) {
+  std::vector<Record *> opDefs = getOpDefinitions(records);
+  std::vector<Record *> typeDefs = getTypeDefinitions(records);
+  std::vector<Record *> attrDefs = getAttrDefinitions(records);
+
+  return false;
+}
+
+int main(int argc, char **argv) {
+  llvm::InitLLVM y(argc, argv);
+  cl::ParseCommandLineOptions(argc, argv);
+
+  return TableGenMain(argv[0], &MlirTableGenStatsMain);
+}
--- utils/build.sh
+++ utils/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd build
 ninja check-dyn
+ninja tblgen-extract
