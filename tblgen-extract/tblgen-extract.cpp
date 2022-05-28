ec08d497431b4654600df0b6b7bad38fc0ff43a7
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-05-28 05:17:29 +0200

    Add extraction of operands and results

--- tblgen-extract/tblgen-extract.cpp
+++ tblgen-extract/tblgen-extract.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "Dyn/Dialect/IRDL/IR/IRDL.h"
+#include "Dyn/Dialect/IRDL/IR/IRDLAttributes.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
@@ -45,11 +46,68 @@ std::vector<Record *> getAttrDefinitions(const RecordKeeper &recordKeeper) {
   return recordKeeper.getAllDerivedDefinitions("AttrDef");
 }
 
-bool MlirTableGenStatsMain(raw_ostream &os, RecordKeeper &records) {
+/// Extract an operation to IRDL.
+void extractOperation(OpBuilder &builder, tblgen::Operator &tblgenOp,
+                      RecordKeeper &records) {
+  auto ctx = builder.getContext();
+  auto op = builder.create<irdl::OperationOp>(
+      UnknownLoc::get(ctx), StringAttr::get(ctx, tblgenOp.getOperationName()));
+
+  // Add the block in the region
+  auto &opBlock = op.body().emplaceBlock();
+  builder.setInsertionPoint(&opBlock, opBlock.begin());
+
+  // Extract operands
+  SmallVector<Attribute> operands;
+  for (auto &tblgenOperand : tblgenOp.getOperands()) {
+    auto operand = NamedTypeConstraintAttr::get(
+        ctx, tblgenOperand.name, AnyTypeConstraintAttr::get(ctx));
+    operands.push_back(operand);
+  }
+  auto irdlOperands = ArrayAttr::get(ctx, operands);
+  builder.create<OperandsOp>(UnknownLoc::get(ctx), irdlOperands);
+
+  // Extract results
+  SmallVector<Attribute> results;
+  for (auto &tblgenResult : tblgenOp.getResults()) {
+    auto result = NamedTypeConstraintAttr::get(ctx, tblgenResult.name,
+                                               AnyTypeConstraintAttr::get(ctx));
+    results.push_back(result);
+  }
+  auto irdlResults = ArrayAttr::get(ctx, results);
+  builder.create<ResultsOp>(UnknownLoc::get(ctx), irdlResults);
+
+  // Put the insertion point after the created operation.
+  builder.setInsertionPointAfter(op);
+  assert(succeeded(op.verify()));
+}
+
+/// Extract the dialect to IRDL
+void extractDialect(OpBuilder &builder, RecordKeeper &records) {
+  auto ctx = builder.getContext();
   std::vector<Record *> opDefs = getOpDefinitions(records);
   std::vector<Record *> typeDefs = getTypeDefinitions(records);
   std::vector<Record *> attrDefs = getAttrDefinitions(records);
 
+  // Retrieve the dialect name.
+  assert(opDefs.size() > 0);
+  auto dialectName = tblgen::Operator(opDefs[0]).getDialectName();
+
+  // Create the IDRL dialect operation, and set the insertion point in it.
+  auto dialect = builder.create<irdl::DialectOp>(
+      UnknownLoc::get(ctx), StringAttr::get(ctx, dialectName));
+  auto &dialectBlock = dialect.body().emplaceBlock();
+  builder.setInsertionPoint(&dialectBlock, dialectBlock.begin());
+
+  // Walk all TableGen operations, and create new IRDL operations.
+  for (auto rec : opDefs) {
+    // Create the operation using the TableGen name.
+    auto tblgenOp = tblgen::Operator(rec);
+    extractOperation(builder, tblgenOp, records);
+  }
+}
+
+bool MlirTableGenStatsMain(raw_ostream &os, RecordKeeper &records) {
   // Create the context, and the main module operation.
   MLIRContext ctx;
   ctx.getOrLoadDialect<IRDLDialect>();
@@ -61,31 +119,7 @@ bool MlirTableGenStatsMain(raw_ostream &os, RecordKeeper &records) {
   auto &moduleBlock = module->getRegion().getBlocks().front();
   builder.setInsertionPoint(&moduleBlock, moduleBlock.begin());
 
-  // Retrieve the dialect name.
-  assert(opDefs.size() > 0);
-  auto dialectName = tblgen::Operator(opDefs[0]).getDialectName();
-
-  // Create the IDRL dialect operation, and set the insertion point in it.
-  auto dialect = builder.create<irdl::DialectOp>(
-      unknownLoc, StringAttr::get(&ctx, dialectName));
-  auto &dialectBlock = dialect.body().emplaceBlock();
-  builder.setInsertionPoint(&dialectBlock, dialectBlock.begin());
-
-  // Walk all TableGen operations, and create new IRDL operations.
-  for (auto rec : opDefs) {
-    // Create the operation using the TableGen name.
-    auto tblgenOp = tblgen::Operator(rec);
-    auto op = builder.create<irdl::OperationOp>(
-        unknownLoc, StringAttr::get(&ctx, tblgenOp.getOperationName()));
-
-    // Add the block in the region
-    auto &opBlock = op.body().emplaceBlock();
-    builder.setInsertionPoint(&opBlock, opBlock.begin());
-
-    // Put the insertion point after the created operation.
-    builder.setInsertionPointAfter(op);
-    assert(succeeded(op.verify()));
-  }
+  extractDialect(builder, records);
 
   module->print(os);
 
