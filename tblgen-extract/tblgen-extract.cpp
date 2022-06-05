051209c9f8ecad30357e9f7fb300a22cbe727c5f
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-06-05 18:12:24 +0200

    [extract] Add utils to manipulate constraint strs

--- tblgen-extract/tblgen-extract.cpp
+++ tblgen-extract/tblgen-extract.cpp
@@ -52,6 +52,60 @@ std::vector<Record *> getAttrDefinitions(const RecordKeeper &recordKeeper) {
   return recordKeeper.getAllDerivedDefinitions("AttrDef");
 }
 
+/// Check that parentheses are balanced
+bool hasBalancedParentheses(StringRef str) {
+  int parenLevel = 0;
+  for (auto c : str) {
+    if (c == '(') {
+      parenLevel += 1;
+    } else if (c == ')') {
+      parenLevel -= 1;
+      if (parenLevel < 0)
+        return false;
+    }
+  }
+  return parenLevel == 0;
+}
+
+/// Remove outer parentheses if they exists.
+StringRef removeOuterParentheses(StringRef str) {
+  str = str.trim();
+  if (str.front() != '(' || str.back() != ')')
+    return str;
+  auto simplifiedStr = str.slice(1, str.size() - 1);
+  if (hasBalancedParentheses(simplifiedStr))
+    return simplifiedStr;
+  return str;
+}
+
+/// Split a predicate on the given operator.
+/// For instance, `separateOnOperator("a || b", "||")` will return
+/// `{"a", "b"}`.
+Optional<std::pair<StringRef, StringRef>> separateOnOperator(StringRef str,
+                                                             StringRef op) {
+  auto parenLevel = 0;
+  for (size_t idx = 0; idx < str.size(); idx++) {
+    auto c = str[idx];
+    if (c == '(')
+      parenLevel += 1;
+    if (c == ')') {
+      parenLevel -= 1;
+      assert(parenLevel >= 0);
+    }
+    if (parenLevel == 0 && str.slice(idx, idx + op.size()) == op)
+      return {{str.slice(0, idx), str.slice(idx + op.size(), str.size())}};
+  }
+  return {};
+}
+
+Attribute extractConstraint(MLIRContext *ctx,
+                            const tblgen::Constraint &constraint) {
+  std::string pred = constraint.getPredicate().getCondition();
+  StringRef simpPred = StringRef(pred).trim();
+  llvm::errs() << "Cannot resolve constraint:" << simpPred << "\n";
+  return AnyTypeConstraintAttr::get(ctx);
+}
+
 /// Extract an operation to IRDL.
 void extractOperation(OpBuilder &builder, tblgen::Operator &tblgenOp,
                       RecordKeeper &records) {
@@ -66,8 +120,9 @@ void extractOperation(OpBuilder &builder, tblgen::Operator &tblgenOp,
   // Extract operands
   SmallVector<Attribute> operands;
   for (auto &tblgenOperand : tblgenOp.getOperands()) {
-    auto operand = NamedTypeConstraintAttr::get(
-        ctx, tblgenOperand.name, AnyTypeConstraintAttr::get(ctx));
+    auto constraint = extractConstraint(ctx, tblgenOperand.constraint);
+    auto operand =
+        NamedTypeConstraintAttr::get(ctx, tblgenOperand.name, constraint);
     operands.push_back(operand);
   }
   auto irdlOperands = ArrayAttr::get(ctx, operands);
@@ -76,8 +131,9 @@ void extractOperation(OpBuilder &builder, tblgen::Operator &tblgenOp,
   // Extract results
   SmallVector<Attribute> results;
   for (auto &tblgenResult : tblgenOp.getResults()) {
-    auto result = NamedTypeConstraintAttr::get(ctx, tblgenResult.name,
-                                               AnyTypeConstraintAttr::get(ctx));
+    auto constraint = extractConstraint(ctx, tblgenResult.constraint);
+    auto result =
+        NamedTypeConstraintAttr::get(ctx, tblgenResult.name, constraint);
     results.push_back(result);
   }
   auto irdlResults = ArrayAttr::get(ctx, results);
