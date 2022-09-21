36258e4dc55974b84896ef11f37fa8501e99dc60
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-09-21 16:29:15 +0100

    Use better linker

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+if command -v mold > /dev/null; then
+  BEST_LINKER="mold"
+elif command -v lld > /dev/null; then
+  BEST_LINKER="lld"
+else
+  BEST_LINKER="ld"
+fi
+LINKER=${3:-${BEST_LINKER}}
+
 mkdir -p build && cd build
+
 export CMAKE_GENERATOR=Ninja
-cmake -G Ninja ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DMLIR_DIR=$(pwd)/../llvm-project/build/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$(pwd)/../llvm-project/build/bin/llvm-lit
+cmake -G Ninja ../ \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+      -DMLIR_DIR=$(pwd)/../llvm-project/build/lib/cmake/mlir \
+      -DLLVM_EXTERNAL_LIT=$(pwd)/../llvm-project/build/bin/llvm-lit \
+      -DLLVM_USE_LINKER=${LINKER}
