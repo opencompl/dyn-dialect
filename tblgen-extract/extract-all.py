7521998c398a8834a6207dfee76ac8f813727395
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-10-02 19:04:59 +0200

    Allow to pass additionnal arguments to the tablegen extractor

--- tblgen-extract/extract-all.py
+++ tblgen-extract/extract-all.py
@@ -9,6 +9,7 @@ tblgen_extract_bin = "build/bin/tblgen-extract"
 
 def generate_irdl_file(file: str,
                        output_folder: str,
+                       args: list[str] = [],
                        print_command: bool = True) -> None:
     """
     Read a TableGen file and translate it to an IRDL file in the given folder
@@ -19,7 +20,7 @@ def generate_irdl_file(file: str,
     command = [
         f"./{tblgen_extract_bin}", f"{os.path.join(root, file)}",
         f"--I={llvm_root}/mlir/include", f"--I={root}"
-    ]
+    ] + args
     res = subprocess.run(command, capture_output=True)
     if res.returncode != 0:
         return None
@@ -31,16 +32,18 @@ def generate_irdl_file(file: str,
     output_file.close()
 
 
-def generate_all_irdl_files(output_folder: str) -> None:
+def generate_all_irdl_files(output_folder: str, args: list[str]) -> None:
     for root, _, files in os.walk(llvm_root + "/mlir/include"):
         for file in files:
             if file.endswith(".td"):
-                generate_irdl_file(os.path.join(root, file), output_folder)
+                generate_irdl_file(os.path.join(root, file), output_folder, args=args)
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise Exception(
             "Expected at least one command line argument: the "
-            "path of the folder in which the IRDL files should be extracted.")
-    files = generate_all_irdl_files(sys.argv[1])
+            "path of the folder in which the IRDL files should be extracted. "
+            "The following arguments are the arguments that will be passed to "
+            "the tblgen-extract binary.")
+    files = generate_all_irdl_files(sys.argv[1], sys.argv[2:])
